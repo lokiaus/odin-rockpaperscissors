b02d9d47ab68d96597782334a3efb53b213b8add
lokiaus <126240945+lokiaus@users.noreply.github.com>
2023-04-12 04:25:35 +1000

    created playRound function and styled index.html

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ function getComputerChoice() {
     //generate a random integer from 0 to 2
     let guess = Math.floor(Math.random() * 3);
     if (guess == 0) {
-        return "Rock"
+        return "rock"
     } else if (guess == 1) {
-        return "Paper"
+        return "paper"
     } else if (guess == 2) {
-        return "Scissors"
+        return "scissors"
     }
 }
 
-let computerChoice = getComputerChoice()
-console.log(computerChoice)
+function playRound (playerChoice, computerChoice) {
+    //rock>scissors, paper>rock, scissors>paper
+    if (playerChoice == computerChoice) {
+        return "tie"
+    } else if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper") {
+        return "victory!"
+    } else {
+        return "defeated."
+    }
+}
+
+const computerChoice = getComputerChoice();
+const playerChoice = prompt('Your choice: ').toLowerCase();
+console.log(`Your choice was ${playerChoice}. The computer's choice was ${computerChoice}.`)
+console.log(playRound(playerChoice, computerChoice));
