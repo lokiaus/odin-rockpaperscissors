a83c8f48e1d413ff82895d54089b005ecda7456b
lokiaus <126240945+lokiaus@users.noreply.github.com>
2023-04-12 05:46:02 +1000

    added game function

--- script.js
+++ script.js
@@ -13,15 +13,59 @@ function getComputerChoice() {
 function playRound (playerChoice, computerChoice) {
     //rock>scissors, paper>rock, scissors>paper
     if (playerChoice == computerChoice) {
-        return "tie"
-    } else if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper") {
-        return "victory!"
-    } else {
-        return "defeated."
+        return 0 // Tie
+    } else if (playerChoice == "scissors") {
+        if (computerChoice == "paper") {
+            console.log("Victory! Scissors beat paper.")
+            return 1 // Win
+        } else {
+            console.log("Defeat. Rock beat scissors.")
+            return 2 // Loss
+        }
+    } else if (playerChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("Victory! Paper beat rock.")
+            return 1
+        } else {
+            console.log("Defeat. Scissors beat paper.")
+            return 2
+        }
+    } else if (playerChoice == "rock") {
+        if (computerChoice == "scissors") {
+            console.log("Victory! Rock beat scissors.")
+            return 1
+        } else {
+            console.log("Defeat. Paper beat rock.")
+            return 2
+        }
     }
 }
 
-const computerChoice = getComputerChoice();
-const playerChoice = prompt('Your choice: ').toLowerCase();
-console.log(`Your choice was ${playerChoice}. The computer's choice was ${computerChoice}.`)
-console.log(playRound(playerChoice, computerChoice));
+function game() {
+    let playerWins = 0
+    let ComputerWins = 0
+    let computerChoice = ""
+    let playerChoice = ""
+    let result = ""
+    for (let gamesPlayed = 0; gamesPlayed < 5; gamesPlayed++) {
+        computerChoice = getComputerChoice();
+        playerChoice = prompt('Your choice: ').toLowerCase();
+        result = playRound(playerChoice, computerChoice);
+        if (result == 1) {
+            playerWins++
+        } else if (result == 2) {
+            ComputerWins++
+        } else {
+            console.log("Tie.")
+        }
+    }
+    if (playerWins > ComputerWins) {
+        console.log(`Player Wins ${playerWins} to ${ComputerWins}!`)
+    } else if (playerWins < ComputerWins) {
+        console.log(`Computer Wins ${ComputerWins} to ${playerWins}!`)
+    } else {
+        console.log(`Game tied ${playerWins} to ${ComputerWins}.`)
+    }
+}
+
+game();
